93ce4336fb0ffb81f3002d2b91429c7b017ba4c1
Edwin <edwin.limaa@gmail.com>
2020-05-24 11:30:44 -0300

    Criando Componente Home

--- cliente-app/src/app/app.module.ts
+++ cliente-app/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { ClientesModule } from './clientes/clientes.module'
 import { TemplateModule } from './template/template.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
